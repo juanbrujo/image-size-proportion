84a3eda7fc980eba1aea7e5050779aace076911c
Jorge Epuñan <juanbrujo@gmail.com>
2026-02-14 17:41:04 -0300

    starting all again

--- tests/unit/Calculator.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Calculator from "@/components/Calculator.vue"
-
-describe("Calculator", () => {
-  it("renders", () => {
-    const wrapper = mount(Calculator)
-    expect(wrapper.contains('div')).toBe(true)
-  })
-})
